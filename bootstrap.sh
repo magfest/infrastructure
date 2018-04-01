e8feee89a4438068cb73d7611e4821171a19956e
Rob Ruana <rob@robruana.com>
2018-03-31 23:39:38 -0400

    salt cloud

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 git clone --depth 1 https://github.com/magfest/infrastructure-secret.git .secret
 rsync -avhu --progress --exclude '.git' .secret/ ./
 curl -L https://bootstrap.saltstack.com | sudo sh -s -- -P -M stable
-salt-call --id=salt-master --file-root=salt --pillar-root=pillar state.highstate
+salt-call --local --id=salt-master --file-root=salt --pillar-root=pillar state.highstate
 salt-key -y -a salt-master
