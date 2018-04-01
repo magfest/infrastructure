5fb4dd638c96c1855bdc5e39a737f02a81cf03d2
Rob Ruana <rob@robruana.com>
2018-04-01 00:17:01 -0400

    salt cloud

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,37 @@
 #! /bin/bash
 
-git clone --depth 1 https://github.com/magfest/infrastructure-secret.git .secret
-rsync -avh --progress --ignore-existing --exclude '.git' .secret/ ./
-curl -L https://bootstrap.saltstack.com | sudo sh -s -- -P -M stable
-salt-call --local --id=salt-master --file-root=salt --pillar-root=pillar state.highstate
-salt-key -y -a salt-master
+CURRENT_DIR = `cwd`
+
+# Download the infrastructure code
+git clone --depth 1 https://github.com/magfest/infrastructure.git /tmp/infrastructure
+git clone --depth 1 https://github.com/magfest/infrastructure-secret.git /tmp/infrastructure-secret
+
+# Copy the secrets into our temp infrastructure dir
+rsync -avh --progress --ignore-existing --exclude '.git' /tmp/infrastructure-secret ./
+
+# Install SaltStack master and minion
+curl -L https://bootstrap.saltstack.com | sudo sh -s -- -i 'salt-master' -P -M stable
+
+# Run salt locally to configure salt-master
+cd /tmp/infrastructure
+salt-call --local --id='salt-master' --file-root=salt --pillar-root=pillar state.highstate
+
+# Preseed salt-master's minion key
+salt-key --gen-keys='salt-master'
+mkdir -p /etc/salt/pki/master/minions/
+cp salt-master.pub /etc/salt/pki/master/minions/
+mkdir -p /etc/salt/pki/minion/
+cp salt-master.pem /etc/salt/pki/minion/minion.pem
+cp salt-master.pub /etc/salt/pki/minion/minion.pub
+
+# Restart the services
+/etc/init.d/salt-master restart
+/etc/init.d/salt-minion restart
+
+# Tell the salt-master's minion to configure itself
+salt 'salt-master' state.highstate
+
+# Cleanup
+cd $CURRENT_DIR
+rm -r /tmp/infrastructure
+rm -r /tmp/infrastructure-secret
