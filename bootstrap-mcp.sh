0ae16b04f39de01972c5e7a08728ae65371927c7
Rob Ruana <rob@robruana.com>
2018-04-24 14:05:33 -0400

    bootstrap

--- bootstrap-mcp.sh
+++ bootstrap-mcp.sh
@@ -3,25 +3,24 @@
 # This script will bootstrap and configure the salt-master server
 # used to manage the entire MAGFest IT infrastructure.
 
-# Download the infrastructure code
-git clone --depth 1 https://github.com/magfest/infrastructure.git /srv/infrastructure
-
-# Change into our temp infrastructure bootstrap dir
-cd /srv/infrastructure
+# Change into tmp dir
+cd /tmp
 
 # Install SaltStack master and minion
-# TODO: Commit 19ec7b6de18256dd9b52919ef9c0d8b39d874277 contains a fix for
-#       docker_containers that we need, and newer versions contain bugs that
-#       we can't work around. Update this to use "stable" once the next
-#       version after v2018.3.0 is released.
-curl -o /tmp/bootstrap-salt.sh -L https://bootstrap.saltstack.com
-sh /tmp/bootstrap-salt.sh -i 'mcp' -L -M -P git 19ec7b6de18256dd9b52919ef9c0d8b39d874277
+curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com
+sh bootstrap-salt.sh -i 'mcp' -L -M -P stable
 
 # Preseed mcp's minion key
 salt-key --gen-keys='mcp'
 cp mcp.pub /etc/salt/pki/master/minions/mcp
 cp mcp.pem /etc/salt/pki/minion/minion.pem
 cp mcp.pub /etc/salt/pki/minion/minion.pub
+rm mcp.pem
+rm mcp.pub
+
+# Download the infrastructure code
+git clone --depth 1 https://github.com/magfest/infrastructure.git /srv/infrastructure
+cd /srv/infrastructure
 
 # Run salt locally to configure a minimal mcp
 salt-call --local --id='bootstrap' --file-root=salt --pillar-root=pillar state.highstate
