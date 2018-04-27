0a5eda96553d8fdeb634564a36449df1857ecf68
Rob Ruana <rob@robruana.com>
2018-04-27 14:13:47 -0400

    magbot

--- salt/legacy_magbot/secret.sh
+++ salt/legacy_magbot/secret.sh
@@ -1,11 +1,11 @@
-export HUBOT_AUTH_ADMIN="{{ salt['pillar.get']('auth_admin', '') }}"
-export HUBOT_SLACK_TOKEN="{{ salt['pillar.get']('slack_token', '') }}"
+export HUBOT_AUTH_ADMIN="{{ salt['pillar.get']('magbot:auth_admin', '') }}"
+export HUBOT_SLACK_TOKEN="{{ salt['pillar.get']('magbot:slack_token', '') }}"
 
-export PORT="{{ salt['pillar.get']('port', '') }}"
+export PORT="{{ salt['pillar.get']('magbot:port', '') }}"
 
-export HUBOT_JIRA_URL="{{ salt['pillar.get']('jira_url', '') }}"
-export HUBOT_JIRA_USER="{{ salt['pillar.get']('jira_user', '') }}"
-export HUBOT_JIRA_PASSWORD="{{ salt['pillar.get']('jira_password', '') }}"
-export HUBOT_JIRA_IGNOREUSERS="{{ salt['pillar.get']('jira_ignoreusers', '') }}"
+export HUBOT_JIRA_URL="{{ salt['pillar.get']('magbot:jira_url', '') }}"
+export HUBOT_JIRA_USER="{{ salt['pillar.get']('magbot:jira_user', '') }}"
+export HUBOT_JIRA_PASSWORD="{{ salt['pillar.get']('magbot:jira_password', '') }}"
+export HUBOT_JIRA_IGNOREUSERS="{{ salt['pillar.get']('magbot:jira_ignoreusers', '') }}"
 
 # export HUBOT_LOG_LEVEL="debug"
